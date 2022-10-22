a1eb0095eae30ef3ee362cc04e98599c11d80b84
anandece348 <andndece2608@gmail.com>
2022-10-22 13:32:07 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+import { shallow } from "enzyme";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+var Shape = require('./shape.js');
+class Circle extends Shape{
+    constructor(color){
+        super();
+    }
+    calculateArea(){
+
+    }
+}
+module.exports = Circle;
+class Shape{
+    constructor(){
+        this.color = "green";
+    }
+    drawShape(){
+
+    }
+    calculateArea(){
+
+    }
+    
+}
+module.exports = Shape;
